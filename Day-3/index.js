6d4bd691ce5fa743742c901bef7515bfdc640f49
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-07-01 15:11:50 +0530

    variables

---
+++ Day-3/index.js
@@ -0,0 +1,60 @@
+// console.log("This is external Javascript");
+/**
+ * 
+ * variables in js:-
+ *   it acts as a containers to store the datatypes to perform particular operations
+ * 
+ * types of variables:-
+ *   ->var
+ *   ->let
+ *   ->const
+ *   ->automatic
+ * 
+ * syntax:-
+ * variable          variableName= value;
+ *  keywords          identifier
+ * 
+ * Rules for variables;-
+ * can not start with numbers
+ * cannot start with keywords as a identifiers
+ * we can sgtart with special cheracters like _ and $
+ * 
+ * 
+ */          
+
+// var a=34;//intilization
+// combination of declaration and assignement to thevariable
+
+// var a;//declaration
+//diclaring a variable  with variableName/identifier without assigning a value
+
+//  a=34;//assignement
+ //diclaring a value to the identifier without keyword which is ready to use
+
+
+
+
+ 
+
+ 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
