499f98876a0dd9a3d4bd6d5962a6421269329a25
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-07-02 15:09:17 +0530

    GEC DataTypes

---
+++ Day-4/index.js
@@ -0,0 +1,55 @@
+/**
+ * hoisting:-
+ * hoisting a is process of accessing a variables functions class without excuting a scopes
+ * GEC:-
+ * GEC stands for global exicution context
+ * it is a starting point and ending point of javascript code
+ * you can write the code outside the function is taken care by GEC
+ * function inside code will taken care by the FEC
+ * in this GEC they are two phases
+ * 
+ * 
+ * 
+ * Datatypes:-
+ * premative data types
+ *
+ * the data types are immutable,i.e, The value cannot changed once assigning to a variabl
+ * 
+ * Number
+ * String
+ * Boolean
+ * undefined
+ * null
+ * symbol
+ * 
+ * Non-Premative datatypes
+ * 
+ * the data types are mutabale i.e, the value can be changed 
+ * 
+ * arrays
+ * objects
+ * functions
+ * classes
+ * set
+ * Regular expressions
+ * etc.,
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
