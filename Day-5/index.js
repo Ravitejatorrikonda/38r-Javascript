a9246d84f374b0104db61b56c058de0a174f348a
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-07-07 16:58:16 +0530

    Operatores

---
+++ Day-5/index.js
@@ -0,0 +1,66 @@
+/**
+ * data types
+ * GEC
+ * 
+ * Operatores
+ * 
+ * in javascript there is a special symbols to perform particulare operation 
+ * types of operatores:-
+ * 
+ * 1.arithamtic operatore
+ *  to perform mathamatical expressions
+ * + - * / % ++ --
+ * 2.assignment operatore
+ * To assigning a value to the variable
+ * 3.comparision operatore
+ * 4.logical operatore
+ * 5. Terinery operatore
+ * 
+ */
+
+// comparision operatore
+// < > <= >= == === != 
+// comapring a values to return a boolean values
+
+// var age=prompt("enter a number")
+
+// var age1=prompt("enter a number")
+// console.log(age*age1);
+// type conversion
+// implicit
+//Explacit
+// String();
+// Boolean()
+console.clear()
+//  var a=+prompt("enter a number")
+//  var b=+prompt("enter a number")
+//  console.log(a+b);
+ console.clear();
+
+//  logical operatore
+// && || ! 
+
+// condition && condition
+
+// ternary operatore
+//it is a shorthand property of if else
+
+// condition?expression 1 : expression2;
+var age=18;
+var status=(age>=18)? "adult" : "minore"
+console.log(status);
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
