893c0d04f8d31a5790296346065b067aa7394669
Ravitejatorrikonda <ravitejatorrikonda@gmail.com>
2025-08-26 17:29:07 +0530

    filter

---
+++ Day-23/index.js
@@ -0,0 +1,63 @@
+/**
+ * 
+ * map forEach
+ */
+
+// let arr=[1,2,3,4,5,undefined,,]
+// function print(){
+//     console.log("Hello there")
+// }
+// arr.forEach(print)
+
+
+
+// 1,4,9,16,25
+// let res=[]
+// for(i=0;i<arr.length;i++){
+    //     res.push(arr[i]*arr[i])
+    // }
+    // // console.log(res)
+    // let res=arr.map((ele)=>ele**2)
+    // console.log(res)
+    //odd,even,odd,even,odd
+    // let arr=[1,2,3,4,5]
+
+    // let res=[]
+    // for(let i=0;i<arr.length;i++){
+    //     if(arr[i]%2==0){
+    //              res.push("even")
+    //     }else{
+    //         res.push("odd")
+    //     }
+    // }
+    // console.log(res)
+    // let res=arr.map(ele=>ele%2==0?"even":"odd")
+    // console.log(res)
+
+    //Filter
+    // 
+    let arr=[1,2,3,4,5]
+//     //[2,4]
+//    let res= arr.filter(ele=>ele%2==0)
+//    console.log(res)
+//    console.log(arr.map(ele=>ele%2==0))
+
+// function x(ele){
+// return ele%2==0
+// }
+// console.log(arr.filter(x))
+// let promise1=new Promise((resolve,reject)=>{
+//     resolve("hello there")
+// })
+// function x(){
+//     return promise1.then(ele=>ele)
+// }
+// // console.log(x())
+// x().then(ele=>console.log(ele))
+
+// function x(){
+//     return Promise.resolve("hello")
+// }
+// console.log(x().then(res=>console.log(res)))
+// console.log(Promise.resolve("hello").then(data=>console.log(data)));
+
